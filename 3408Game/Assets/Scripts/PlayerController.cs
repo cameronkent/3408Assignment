a8fc81f62a71715c299ac0b8c9c6cdf5821de15d
cameron-kent <45619337+cameron-kent@users.noreply.github.com>
2019-04-09 20:55:00 +1000

    Worked on Player Character
    Imported Character sprites for player
    Began building Player object
    Began building player animations

---
+++ 3408Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float maxSpeed = 10f;
+    bool facingRight = true;
+
+    public Animator anim;
+
+    bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+    public float jumpForce = 700f;
+
+    private Rigidbody2D rigidBody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Ground", false);
+            rigidBody2D.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    // Update is called on Physics change
+    private void FixedUpdate()
+    {
+        //check to see if player is touching ground
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", grounded);
+
+        anim.SetFloat("vSpeed", rigidBody2D.velocity.y);
+
+        float move = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", Mathf.Abs(move));
+
+        rigidBody2D.velocity = new Vector2(move * maxSpeed, rigidBody2D.velocity.y);
+
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
+    }
+
+    //Change direction sprite faces on change in movement direction
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
