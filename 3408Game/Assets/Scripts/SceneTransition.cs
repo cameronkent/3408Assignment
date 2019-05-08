4a07fa3cdb6cc9bc1fb5b8aa1db202f94a9d7948
janeGzan <jane.ganzan@my.jcu.edu.au>
2019-05-08 23:52:32 +1000

    Add level transition

---
+++ 3408Game/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public Animator animator;
+    public string sceneName;
+    public bool levelComplete;
+    public Canvas uiCanvas;
+    
+    // Update is called once per frame
+
+   
+    void Update()
+    {
+        if (levelComplete)
+        {
+            uiCanvas.enabled = false;
+            SceneManager.LoadScene(sceneName);
+            StartCoroutine(LoadScene());
+        }
+        else
+        {
+            uiCanvas.enabled = true;
+        }
+    }
+
+    IEnumerator LoadScene()
+    {
+        animator.SetTrigger("end");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
