9efac18561af45c3b36f0a1732edbe2470a6f350
janeGzan <jane.ganzan@my.jcu.edu.au>
2019-05-08 15:55:55 +1000

    Add timer

---
+++ 3408Game/Assets/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text textTime;
+    public float timeLeft = 30.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textTime.text = ((int)timeLeft).ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        textTime.text = ((int)timeLeft).ToString();
+        if (timeLeft < 0)
+        {
+
+        }
+    }
+}
