5fc8793dd21a48f578c1de844ad569f7f5b185d4
janeGzan <jane.ganzan@my.jcu.edu.au>
2019-05-08 23:02:08 +1000

    Add key in UI

---
+++ 3408Game/Assets/Scripts/Key.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Key : MonoBehaviour
+{
+    public Image keyImage;
+    public bool hasKey;
+
+    void Start()
+    { 
+
+    }
+
+    void FixedUpdate()
+    {
+        float alphaValue;
+        if (hasKey)
+        {
+            alphaValue = 1f;
+        }
+        else
+        {
+            alphaValue = 0f;
+        }
+        keyImage.color = new Color(keyImage.color.r, keyImage.color.g, keyImage.color.b, alphaValue);
+    }
+}
