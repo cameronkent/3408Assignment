63a238a51af2b9cf905ffa7b08c8ab0996596817
cameron-kent <45619337+cameron-kent@users.noreply.github.com>
2019-05-09 21:43:42 +1000

    player work
    started working on climbing

--- 3408Game/Assets/Scripts/PlayerMovement.cs
+++ 3408Game/Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,15 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
     public Animator animator;
+    public float distance;
+    public LayerMask whatIsLadder;
 
     private Rigidbody2D rigidbody;
 
     public float runSpeed = 30f;
     float horizontalMove = 0f;
+    float inputVertical;
+    bool isClimbing;
     bool jump = false;
     bool crouch = false;
     bool isArmed = false;
@@ -21,7 +25,7 @@ public class PlayerMovement : MonoBehaviour
         rigidbody = GetComponent<Rigidbody2D>();
     }
 
-        private void Update()
+    private void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
@@ -86,5 +90,30 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.up, distance, whatIsLadder);
+
+        if (hitInfo.collider != null)
+        {
+            if(Input.GetButtonDown("Climb"))
+            {
+                isClimbing = true;
+                animator.SetBool("IsClimbing", true);
+            }
+        } else
+        {
+            isClimbing = false;
+            animator.SetBool("IsClimbing", false);
+        }
+
+        if (isClimbing == true)
+        {
+            inputVertical = Input.GetAxisRaw("Vertical");
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, inputVertical * runSpeed);
+            rigidbody.gravityScale = 0;
+        } else
+        {
+            rigidbody.gravityScale = 1;
+        }
     }
 }
